af139731d3e0d4e3bc9b9b919836a4ab1acbd556
Lukáš Bubník <lukas.bu@seznam.cz>
2018-09-11 16:44:13 +0200

    Added models

---
+++ BookManager/Models/Author.cs
@@ -0,0 +1,7 @@
+﻿namespace BookManager.Models {
+    public class Author {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
